a06d95b6602ca0c15723460a2a887e326ef75172
AldinHyuseinov <Aldin6687@gmail.com>
2026-04-10 00:10:01 +0300

    script change

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+# start.sh
+
+if [ -z "$DATABASE_URL" ]; then
+  echo "Error: DATABASE_URL is not set. Check Azure App Settings."
+  exit 1
+fi
 
 echo "Waiting for SQL Server to be ready..."
 sleep 15 
 
-echo "Running Prisma Sync..."
-npx prisma migrate deploy
+echo "Push to db..."
+npx prisma db push --accept-data-loss
 
 echo "Starting Next.js..."
 node server.js
